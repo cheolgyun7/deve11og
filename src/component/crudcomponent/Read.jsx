7a9ae0c112034e082133176cefa70b2f4edfc300
Kim CG <cjfrbs1256@gmail.com>
2024-02-09 12:52:21 +0900

    style: 스타일적용

--- src/component/crudcomponent/Read.jsx
+++ src/component/crudcomponent/Read.jsx
@@ -18,11 +18,10 @@ const Read = () => {
       <span> 좋아요순</span>
       <AlbumBox>
         {albums.map((album) => (
-          <AlbumUi key={album.user_id}>
+          <AlbumArticle key={album.user_id}>
             <AlbumThumnail>{/* <img src={album.album_img} alt='앨범 썸네일' /> */}</AlbumThumnail>
-            <li>{album.title}</li>
-            <li>{album.contents}</li>
-            <li>
+            <h4>{album.title}</h4>
+            <div>
               <span>{album.reg_date}</span>
               <span>
                 <LikeIcon onClick={likeIconClick}>
@@ -30,8 +29,8 @@ const Read = () => {
                 </LikeIcon>
               </span>
               <span>{album.liked}</span>
-            </li>
-          </AlbumUi>
+            </div>
+          </AlbumArticle>
         ))}
       </AlbumBox>
     </MainContents>
@@ -57,14 +56,13 @@ export const MainContents = styled.div`
   }
 `;
 
-export const AlbumUi = styled.ul`
-  display: flex;
+export const AlbumArticle = styled.article`
   flex-wrap: wrap;
   width: 20%;
-  background-color: #282828;
+  background-color: #f1f1f1;
   margin-right: 1rem;
   border-radius: 1rem;
-  padding: 1rem;
+  box-shadow: 0 0 0;
   li {
     width: 100%;
     margin: 0.2rem;
@@ -74,8 +72,6 @@ export const AlbumUi = styled.ul`
   }
 `;
 
-export const AlbumLi = styled.li``;
-
 export const AlbumBox = styled.div`
   display: flex;
 `;
@@ -83,4 +79,5 @@ export const AlbumBox = styled.div`
 export const AlbumThumnail = styled.div`
   width: 100%;
   height: 12rem;
+  background-color: black;
 `;
