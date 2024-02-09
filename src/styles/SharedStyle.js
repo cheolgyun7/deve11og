4b8bb6be4d690ea35868faffda5f86a0bbfb829a
서가희 <kahee430@gmail.com>
2024-02-09 19:36:29 +0900

    fix : 충돌 수정

--- src/styles/SharedStyle.js
+++ src/styles/SharedStyle.js
@@ -1,12 +1,8 @@
 import styled from 'styled-components';
 
-<<<<<<< HEAD
-export const Wrapper = styled.div``;
-=======
 export const Wrapper = styled.div`
   color: black;
 `;
->>>>>>> 123bf14675fd3c45ddfedd64e56592e9884ee418
 
 export const HeaderBox = styled.header`
   position: sticky;
@@ -27,10 +23,7 @@ export const FooterBox = styled.footer`
   left: 0;
   right: 0;
   padding: 1rem 0;
-<<<<<<< HEAD
   background: #f5f5f5;
-=======
->>>>>>> 123bf14675fd3c45ddfedd64e56592e9884ee418
 `;
 
 export const Section = styled.section`
@@ -44,8 +37,4 @@ export const LayoutStyle = styled.div`
   margin: 0 auto;
   width: 100%;
   max-width: 1500px;
-<<<<<<< HEAD
-  color: black;
-=======
->>>>>>> 123bf14675fd3c45ddfedd64e56592e9884ee418
 `;
