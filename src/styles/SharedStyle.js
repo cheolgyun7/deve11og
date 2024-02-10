872d1d673f945088aecbd28a8f5456cd0354bcaf
jisu <ahteostnqls@naver.com>
2024-02-10 22:16:15 +0900

    design: header/footer z-index 추가

--- src/styles/SharedStyle.js
+++ src/styles/SharedStyle.js
@@ -9,6 +9,7 @@ export const HeaderBox = styled.header`
   top: 0;
   left: 0;
   right: 0;
+  z-index: 10;
   padding: 1rem 0;
   background: #ffffff;
 
@@ -23,6 +24,7 @@ export const FooterBox = styled.footer`
   bottom: 0;
   left: 0;
   right: 0;
+  z-index: 10;
   padding: 1rem 0;
   background: #ffffff;
 `;
