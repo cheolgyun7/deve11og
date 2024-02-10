0eb22ef277ebc4ea61f1d4165c8c4c1ba539b6c9
서가희 <kahee430@gmail.com>
2024-02-11 01:32:31 +0900

    feat : 삭제 기능 완료

--- src/component/crudcomponent/Write.jsx
+++ src/component/crudcomponent/Write.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { Section } from 'styles/SharedStyle';
 import { db, storage } from '../../firebase';
-import { addDoc, collection, getDocs, query } from 'firebase/firestore';
-import { getDownloadURL, ref, uploadBytes } from '@firebase/storage';
+import { addDoc, collection, deleteDoc, doc, getDocs, query, updateDoc } from 'firebase/firestore';
+import { ref, uploadBytes } from '@firebase/storage';
 import { uuidv4 } from '@firebase/util';
 
 const Write = () => {
@@ -11,15 +11,14 @@ const Write = () => {
   useEffect(() => {
     const fetchData = async () => {
       const boardData = query(collection(db, 'board'));
-      const querySanpshot = await getDocs(boardData);
+      const querySnapshot = await getDocs(boardData);
 
       const initialBoard = [];
-      querySanpshot.forEach((doc) => {
+      querySnapshot.forEach((doc) => {
         const data = {
           id: doc.id,
           ...doc.data()
         };
-        // console.log('data', data);
         initialBoard.push(data);
       });
       setBoard(initialBoard);
@@ -27,28 +26,28 @@ const Write = () => {
     fetchData();
   }, []);
 
-  // 게시물 state
-  const [boardTitle, setBoardTitle] = useState('');
-  const [boardContent, setBoardContent] = useState();
+  // 게시물 state들
+  const [title, setTitle] = useState('');
+  const [contents, setContents] = useState('');
   const [category, setCategory] = useState('');
-  const [thumbnailImg, setThumbnailImg] = useState('');
+  const [thumbnail, setThumbnail] = useState('');
 
-  // 이미지 ID
-  const imgId = uuidv4();
+  // 이미지 id
+  const thumbnailId = uuidv4();
 
-  // 썸네일 기본 이미지 url, 깃헙에 저장된 이미지를 가져옴
+  // 썸네일 기본 이미지 url(깃헙에 저장된 이미지)
   const defaultImgUrl = 'https://github.com/cheolgyun7/deve11og/raw/dev/src/image/userImage.png';
 
-  // 포커스를 위한 변수들
-  const boardTitleRef = useRef(null);
-  const boardContentRef = useRef(null);
+  // 포커스 변수들
+  const titleRef = useRef(null);
+  const contentsRef = useRef(null);
   const categoryRef = useRef(null);
 
-  // 게시물 제목과 내용 change 이벤트
-  const titleChanged = (e) => setBoardTitle(e.target.value);
-  const contentChanged = (e) => setBoardContent(e.target.value);
+  // 게시물 state change 이벤트
+  const titleChanged = (e) => setTitle(e.target.value);
+  const contentChanged = (e) => setContents(e.target.value);
   const categoryChanged = (e) => setCategory(e.target.value);
-  const thumbnailImgChanged = (e) => setThumbnailImg(e.target.files[0]);
+  const thumbnailChanged = (e) => setThumbnail(e.target.files[0]);
 
   // 게시물 등록일 함수
   const now = new Date();
@@ -57,131 +56,179 @@ const Write = () => {
     month: 'long',
     day: 'numeric',
     hour: 'numeric',
-    hour12: false, // 24시간 형식표기
+    hour12: false, // 24시간 형식 표기
     minute: '2-digit'
   });
 
-  // board
-  const [board, setBoard] = useState([
-    {
-      category: '커뮤니티',
-      cnt: 0,
-      contents: boardContent,
-      liked: 0,
-      regDate,
-      title: boardTitle
-    }
-  ]);
+  const [board, setBoard] = useState([]);
 
-  // 미리보기 이미지 삭제
+  // 이미지 미리보기 삭제 함수
   const imgRemove = () => {
-    setThumbnailImg('');
+    setThumbnail('');
   };
 
-  // 게시물 등록 form
-  const insertBoardForm = async (e) => {
+  // 게시물 등록
+  const addBoardForm = async (e) => {
     e.preventDefault();
-
     const newBoard = {
       cnt: 0,
-      contents: boardContent,
+      contents,
       liked: 0,
-      marked: false,
       regDate,
-      thumbnail: imgId,
-      title: boardTitle,
+      thumbnail: thumbnailId,
+      title,
       category
     };
 
-    if (!boardTitle) {
+    // 유효성 검사
+    if (!title) {
       alert('제목을 입력해 주세요');
-      return boardTitleRef.current.focus();
-    } else if (!boardContent) {
-      alert('제목을 입력해 주세요');
-      return boardContentRef.current.focus();
+      return titleRef.current.focus();
+    } else if (!contents) {
+      alert('내용을 입력해 주세요');
+      return contentsRef.current.focus();
     } else if (!category) {
       alert('카테고리를 선택해 주세요');
       return categoryRef.current.focus();
     }
 
-    alert(`"${boardTitle}" 게시물이 등록되었습니다.`);
-    setBoard([...board, newBoard]);
-    setBoardTitle('');
-    setBoardContent('');
-    setCategory('');
-    imgRemove();
-
     // 파이어베이스 게시물 등록
     const collectionRef = collection(db, 'board');
     await addDoc(collectionRef, newBoard);
 
-    // 스토리지 이미지 등록
-    const imgRef = ref(storage, imgId);
+    setBoard([...board, newBoard]);
+    setTitle('');
+    setContents('');
+    setCategory('');
+    imgRemove();
+
+    // 스토리지에 이미지 등록
+    const imgRef = ref(storage, thumbnailId);
 
     // 회원이 등록한 이미지가 없을경우 기본 이미지 등록 / ** 문제 스토리지에 저장은되지만 이미지 파일로 저장이안됨
-    const imageToUpload = thumbnailImg ? thumbnailImg : defaultImgUrl;
+    const imageToUpload = thumbnail ? thumbnail : defaultImgUrl;
     await uploadBytes(imgRef, imageToUpload);
 
-    // // 스토리지에 저장된 이미지 url 가져오기
-    // const downloadUrl = await getDownloadURL(imgRef);
-    // console.log('downloadUrl', downloadUrl);
+    alert(`"${title}" 게시물이 등록되었습니다.`);
   };
 
-  // 게시물 삭제
+  // 수정
+  const [isUpdate, setIsUpdate] = useState(false); // 수정 모드 여부 스테이트
+  const [prevBoard, setPrevBoard] = useState(''); // 현재 수정 중인 게시물 데이터 저장
+
+  const editingBoard = (item) => {
+    if (window.confirm('게시물을 수정하시겠습니까?')) {
+      setPrevBoard(item);
+      setIsUpdate(true);
+    }
+  };
+  // ** 문제 게시물 등록 폼이랑 수정 폼이랑 동일해서 같이 사용하고싶은데 저장된 데이터를 불러오지만 인풋을 수정해도 화면에 렌더링이 안됨
+  const updateBoardForm = async (e) => {
+    e.preventDefault();
+
+    const updatedBoard = {
+      ...prevBoard,
+      title,
+      contents,
+      category,
+      thumbnail
+    };
+
+    try {
+      const boardRef = doc(db, 'board', board.id);
+      await updateDoc(boardRef, updatedBoard);
+      setBoard(updatedBoard);
+      setPrevBoard(null);
+      setIsUpdate(false);
+    } catch (error) {
+      console.error('수정 실패', error);
+    }
+  };
+
+  const removeBoard = async (id) => {
+    if (window.confirm('게시물을 삭제하시겠습니까?')) {
+      try {
+        const boardRef = doc(db, 'board', id);
+        await deleteDoc(boardRef);
+        setBoard(board.filter((item) => item.id !== id));
+        alert('게시물이 삭제되었습니다.');
+      } catch (error) {
+        console.error('삭제 실패', error);
+      }
+    }
+  };
 
   return (
     <Section>
-      <InsertBoard>
-        <InsertBoardForm onSubmit={insertBoardForm}>
-          <SelectBox value={category} onChange={categoryChanged} ref={categoryRef}>
+      <AddBoard>
+        <AddBoardForm onSubmit={isUpdate ? updateBoardForm : addBoardForm}>
+          <SelectBox value={isUpdate ? prevBoard.category : category} onChange={categoryChanged} ref={categoryRef}>
             <option value="">카테고리를 선택해 주세요</option>
             <option value="discussion">커뮤니티</option>
             <option value="techTalk">질문 및 답변</option>
             <option value="about">About</option>
           </SelectBox>
           <TitleInput
-            value={boardTitle}
+            value={isUpdate ? prevBoard.title : title}
             onChange={titleChanged}
-            ref={boardTitleRef}
+            ref={titleRef}
             type="text"
             placeholder="제목을 입력해 주세요"
           />
+          {console.log('prevBoard.title', prevBoard.title)}
           {/* 회원이 이미지 파일을 업로드 한 경우 미리보기 */}
-          {thumbnailImg ? (
+          {/* 인풋 파일에 이미지를 추가하면 URL.createObjectURL()함수가 이미지를 url로 변환해서 src에 넣어줌 */}
+          {thumbnail ? (
             <PreviewDiv>
-              <img src={URL.createObjectURL(thumbnailImg)} alt="이미지" />
+              <img src={URL.createObjectURL(thumbnail)} alt="이미지" />
               <button onClick={imgRemove}>이미지 삭제</button>
             </PreviewDiv>
           ) : null}
 
           <textarea
-            value={boardContent}
+            value={isUpdate ? prevBoard.contents : contents}
             onChange={contentChanged}
-            ref={boardContentRef}
+            ref={contentsRef}
             placeholder="내용을 입력해 주세요"
           ></textarea>
-          <InsertBtnDiv>
-            <button type="submit">작성 완료</button>
+          <AddBtnDiv>
+            <button type="submit">{isUpdate ? '수정 완료' : '작성 완료'}</button>
             <label htmlFor="thumbnail">
-              <ThumbnailDiv>이미지 추가</ThumbnailDiv>
+              <ThumbnailDiv>{isUpdate ? '이미지 변경' : '이미지 추가'}</ThumbnailDiv>
             </label>
-            <ThumbnailInput onChange={thumbnailImgChanged} type="file" accept="image/*" id="thumbnail" />
-          </InsertBtnDiv>
-        </InsertBoardForm>
-      </InsertBoard>
+            <ThumbnailInput onChange={thumbnailChanged} type="file" accept="image/*" id="thumbnail" />
+          </AddBtnDiv>
+        </AddBoardForm>
+      </AddBoard>
+
+      {/* 수정, 삭제를 위한 테스트 코드 */}
+      {board.map((item) => {
+        return (
+          <div key={item.id}>
+            <img src={item.downloadURL} alt="" />
+            <div>아이디 ***************************{item.id}</div>
+            <div>{item.category}</div>
+            <div>{item.title}</div>
+            <div>{item.contents}</div>
+            <div>{item.regDate}</div>
+            <button onClick={() => editingBoard(item)}>수정</button>
+            <button onClick={() => removeBoard(item.id)}>삭제</button>
+          </div>
+        );
+      })}
     </Section>
   );
 };
 
 export default Write;
 
-const InsertBoard = styled.div`
+const AddBoard = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
 
-const InsertBoardForm = styled.form`
+const AddBoardForm = styled.form`
   width: 75%;
   height: 75vh;
   display: flex;
@@ -240,7 +287,7 @@ const TitleInput = styled.input`
   font-size: 1rem;
 `;
 
-const InsertBtnDiv = styled.div`
+const AddBtnDiv = styled.div`
   width: 20%;
   display: flex;
   flex-direction: row;
