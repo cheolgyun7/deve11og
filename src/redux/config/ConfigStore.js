d23d202eb513a9c833e713e9648e97221ba7a457
jisu <ahteostnqls@naver.com>
2024-02-14 02:51:20 +0900

    feat: 댓글 리덕스 작업

--- src/redux/config/ConfigStore.js
+++ src/redux/config/ConfigStore.js
@@ -2,11 +2,13 @@ import { combineReducers, createStore } from 'redux';
 import list from '../modules/list';
 import board from '../modules/board';
 import user from '../modules/user';
+import comment from '../modules/comment';
 
 const rootReducer = combineReducers({
   list,
   board,
-  user
+  user,
+  comment
 });
 
 const store = createStore(rootReducer);
