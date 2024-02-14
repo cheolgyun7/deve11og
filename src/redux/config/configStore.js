a53313e5e7182df37441a0612ca3e5e6f63ae2cb
jisu <ahteostnqls@naver.com>
2024-02-14 19:18:04 +0900

    remove: comment 리덕스 파일 미사용으로 삭제

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -2,13 +2,11 @@ import { combineReducers, createStore } from 'redux';
 import list from '../modules/list';
 import board from '../modules/board';
 import user from '../modules/user';
-import comment from '../modules/comment';
 
 const rootReducer = combineReducers({
   list,
   board,
-  user,
-  comment
+  user
 });
 
 const store = createStore(rootReducer);
